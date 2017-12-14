7ba5fb4d04cf067796e60ea13e027e6f3dda3489
Jason Metten <metten@gmail.com>
2017-12-14 17:01:02 -0600

    Added two pages

--- standupdesmoines-react/src/App.js
+++ standupdesmoines-react/src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/Homepage";
+import LoginPage from "./components/pages/Loginpage";
 
-const App = () => <div>Hello World!</div>
+const App = () => 
+<div>
+    <Route path = "/" exact component = {HomePage} />
+    <Route path = "/login" exact component = {LoginPage} />
+</div>
 
 export default App;
