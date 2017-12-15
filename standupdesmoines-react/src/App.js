7b36c0280de66917ff10114429ed20df050e35a5
Jason Metten <metten@gmail.com>
2017-12-15 16:10:29 -0600

    Added yarn lock and additional dependencies

--- standupdesmoines-react/src/App.js
+++ standupdesmoines-react/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import HomePage from "./components/pages/Homepage";
-import LoginPage from "./components/pages/Loginpage";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
 const App = () => 
-<div>
+<div className = "ui container">
     <Route path = "/" exact component = {HomePage} />
     <Route path = "/login" exact component = {LoginPage} />
 </div>
