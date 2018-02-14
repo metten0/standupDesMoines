3c8b37ec8d399f7fc070524f387728092f921865
Jason Metten <metten@gmail.com>
2018-02-13 18:52:15 -0600

    Login/Logout installed

--- standupdesmoines-react/src/index.js
+++ standupdesmoines-react/src/index.js
@@ -9,10 +9,16 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore (
     rootReducer,
     composeWithDevTools(applyMiddleware(thunk)));
+
+    if(localStorage.standupdesmoinesJWT) {
+      const user = { token: localStorage.standupdesmoinesJWT }; 
+      store.dispatch(userLoggedIn(user)); 
+    };
 
 ReactDOM.render(
     <BrowserRouter>
